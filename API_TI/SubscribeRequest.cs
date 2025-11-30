521e16d83d5fb859846a950026ec889d29e76a14
IlaiAlanis <99218855+IlaiAlanis@users.noreply.github.com>
2025-11-30 16:32:18 -0600

    Creacion de Dto newsletter, notification, orden,
    Pago adicional

---
+++ API_TI/SubscribeRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_TI.Models.DTOs.NewsletterDTOs
+{
+    public class SubscribeRequest
+    {
+        [Required(ErrorMessage = "Email es requerido")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+    }
+}
