7a123d238cd691d2076caea4ee68d8b7d50dc2d5
IlaiAlanis <99218855+IlaiAlanis@users.noreply.github.com>
2025-11-30 16:28:45 -0600

    Creacion de Dto direccion, error, estado, factura

---
+++ API_TI/EstadoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API_TI.Models.DTOs.EstadoDTOs
+{
+    public class EstadoDto
+    {
+        public int IdEstado { get; set; }
+        public int PaisId { get; set; }
+        public string NombreEstado { get; set; }
+    }
+}
---
+++ API_TI/FacturaDto.cs
@@ -0,0 +1,19 @@
+﻿namespace API_TI.Models.DTOs.FacturaDTOs
+{
+    public class FacturaDto
+    {
+        public int IdFactura { get; set; }
+        public int OrdenId { get; set; }
+        public string NumeroOrden { get; set; }
+        public string TipoFactura { get; set; }
+        public string Serie { get; set; }
+        public int? Folio { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Impuestos { get; set; }
+        public decimal Total { get; set; }
+        public string Uuid { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public DateTime? FechaCancelacion { get; set; }
+
+    }
+}
